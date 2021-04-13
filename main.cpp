1cb96ce90a733c41cbf4b2f00cb57642af6ef097
jshabun <jshab003@ucr.edu>
2021-04-13 01:19:35 -0700

    main merge fix

--- main.cpp
+++ main.cpp
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 #include <iostream>
-=======
-nclude <iostream>
->>>>>>> fb9c2df4dc9a94a5ee8070a600852fb21960a295
 #include <string>
 
 int main()
@@ -10,11 +6,7 @@ int main()
     std::string name;
     std::cout << "What is your name?" << std::endl;
     std::cin >> name;
-<<<<<<< HEAD
     std::cout << "Hello " << name << "!" << std::endl;
-=======
-    std:::cout << "Hello " << name << "!" << std::endl;
->>>>>>> fb9c2df4dc9a94a5ee8070a600852fb21960a295
 
     return 0;
 }
